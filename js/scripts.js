42f4f5ffe95722a78e83df80b5e323fdd7c72d9d
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-01-24 16:00:43 -0800

    add findContact prototype function

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,21 @@ function AddressBook() {
   this.contacts = {};
   this.currentId = 0;
 }
-
 AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
 }
-
 AddressBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 }
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] != undefined) {
+    return this.contacts[id];
+  }
+  return false;
+}
+
 
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
