a11d44f4ac18f711177c46cc80b1fae28397a699
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-01-24 16:03:21 -0800

    add deleteContact prototype function

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,13 @@ AddressBook.prototype.findContact = function(id) {
   }
   return false;
 }
-
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
+}
 
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
