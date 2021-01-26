657385f6085c84b9d6652f391b694439e148d9a0
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-01-25 18:28:15 -0800

    add UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business Logic for AddressBook ---------
 function AddressBook() {
   this.contacts = {};
   this.currentId = 0;
@@ -30,13 +31,26 @@ AddressBook.prototype.updateContact = function(id, contact) {
     this.contacts[id].phoneNumber = contact.phoneNumber;
   }
 }
-
+// Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
 }
-
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+// User Interface Logic ---------
+let addressBook = new AddressBook();
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    console.log(addressBook.contacts);
+  });
+});
