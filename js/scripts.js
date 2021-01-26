9b958d74050a8b2b661675bc20bdbe6b5bb09e3d
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-01-26 10:53:00 -0800

    add address constructor and input forms
    Co-authored-by: Louie Schoenknecht <luisesch97@gmail.com>
    Co-Authored-By: Mikey Kriegel <mikkrieg@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,11 @@ function AddressBook() {
   this.contacts = {};
   this.currentId = 0;
 };
+
+Address.prototype.addAddress = function(address){
+  
+}
+
 AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
@@ -32,16 +37,33 @@ AddressBook.prototype.updateContact = function(id, contact) {
     this.contacts[id].email = contact.email;
   };
 };
+
+
 // Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber, email) {
+function Contact(firstName, lastName, phoneNumber, email, address) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
   this.email = email;
+  this.address = address;
+
 };
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
+
+function Address(street, city, state, zip) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + "\n" + this.city + "," + this.state + " " + this.zip;
+}
+
+
 // User Interface Logic ---------
 let addressBook = new AddressBook();
 
@@ -57,11 +79,16 @@ function displayContactDetails(addressBookToDisplay) {
 // REFACTOR HTML TO ONLY MAKE ONE DOM QUERY
 function showContact(contactId) {
   const contact = addressBook.findContact(contactId);
+  console.log(contact);
   $("#show-contact").show();
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
   $(".email").html(contact.email);
+  $(".street").html(contact.address.street);
+  $(".city").html(contact.address.city);
+  $(".state").html(contact.address.state);
+  $(".zip").html(contact.address.zip);
   let buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" + + contact.id + ">Delete</button>");
@@ -84,11 +111,20 @@ $(document).ready(function() {
     const inputtedLastName = $("input#new-last-name").val();
     const inputtedPhoneNumber = $("input#new-phone-number").val();
     const inputtedEmail = $("input#new-email").val();
+    const inputtedStreet = $("input#new-street").val();
+    const inputtedCity = $("input#new-city").val();
+    const inputtedState = $("input#new-state").val();
+    const inputtedZip = $("input#new-zip").val();
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
     $("input#new-email").val("");
-    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail);
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
+    $("input#new-zip").val("");
+    let newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedZip);
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, newAddress);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
   });
